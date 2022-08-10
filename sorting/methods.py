ab684f5cd8a55a648cccc50f98c8d72d0cc281c7
roadprimus <roadprimus@yandex.ru>
2022-08-10 22:04:20 +0300

    Created heapsort sorting method

--- sorting/methods.py
+++ sorting/methods.py
@@ -36,3 +36,32 @@ def insertion(arr: list[int]) -> list:
                 break
 
     return arr
+
+
+def _max(arr: list[int], length: int, a: int, b: int) -> int:
+    """Find max between values at positions a and b."""
+    return a if a < length and arr[a] > arr[b] else b
+
+
+def swap(arr: list[int], root: int, length: int) -> None:
+    """Reorder subtree from array to heap."""
+    left = 2 * root + 1
+    right = 2 * root + 2
+
+    largest = _max(arr, length, right, _max(arr, length, left, root))
+    if largest != root:
+        arr[largest], arr[root] = arr[root], arr[largest]
+        swap(arr, largest, length)
+
+
+def heapsort(arr: list[int]) -> list:
+    """Run Heapsort sorting method."""
+    length = len(arr)
+    for i in range(length, -1, -1):
+        swap(arr, i, length)
+
+    for i in range(length - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        swap(arr, 0, i)
+
+    return arr
